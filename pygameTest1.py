4feee64cce36e935e0cc7481aef0c466e9b61931
igallea <igor.gallea@etudiant.univ-rennes1.fr>
2024-03-19 10:45:29 +0100

    modif

--- pygameTest1.py
+++ pygameTest1.py
@@ -5,24 +5,48 @@ import sys
 pygame.init()
 
 # nb de cases
-
-LONGUEUR = 10 
-LARGEUR = 10
+LONGUEUR = 5
+LARGEUR = 5
 
 # Définition de la taille de la fenêtre
+TAILLE_CASE = 101
 
-taille_case = 100
+#variable de positionnement du cavalier
+estPose = False
+x = 0
+y = 0
 
 # Couleurs
 blanc = (210, 180, 140)
 noir = (139, 69, 19)
+vert = (0, 255, 0)
+
+#image
+image = pygame.image.load("cavalier.png")
+image = pygame.transform.scale(image, (TAILLE_CASE, TAILLE_CASE))
+
+
+# fonction qui place le cavalier
+
+def initPoseCavalier(x,y):
+    casex = x // TAILLE_CASE
+    casey = y // TAILLE_CASE
+    return (casex*TAILLE_CASE,casey*TAILLE_CASE)
+
 
 # Création de la fenêtre
-fenetre = pygame.display.set_mode((taille_case*LONGUEUR, taille_case*LARGEUR))
+fenetre = pygame.display.set_mode((TAILLE_CASE*LONGUEUR, TAILLE_CASE*LARGEUR))
 pygame.display.set_caption("Échiquier Vierge")
 
 # Boucle principale
 while True:
+
+    # Dessin de l'échiquier
+    fenetre.fill(blanc)
+    for ligne in range(LONGUEUR):
+        for colonne in range(LARGEUR):
+            couleur_case = blanc if (ligne + colonne) % 2 == 0 else noir
+            pygame.draw.rect(fenetre, couleur_case, (colonne * TAILLE_CASE, ligne * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE))
 
     for event in pygame.event.get():
 
@@ -30,14 +54,23 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Dessin de l'échiquier
-    fenetre.fill(blanc)
-    for ligne in range(LONGUEUR):
-        for colonne in range(LARGEUR):
-            couleur_case = blanc if (ligne + colonne) % 2 == 0 else noir
-            pygame.draw.rect(fenetre, couleur_case, (colonne * taille_case, ligne * taille_case, taille_case, taille_case))
+        if event.type == pygame.MOUSEBUTTONDOWN and not estPose and event.button == 1:
+            x,y = initPoseCavalier(event.pos[0], event.pos[1])
+            estPose = True
+
+        if event.type == pygame.MOUSEMOTION and not estPose:
+            x = event.pos[0]
+            y = event.pos[1]
+            
+    if estPose:
+        fenetre.blit(image, (x,y))
+    elif x != 0 and y != 0:
+        pygame.draw.rect(fenetre, vert,(x//TAILLE_CASE*TAILLE_CASE,y//TAILLE_CASE*TAILLE_CASE, TAILLE_CASE,TAILLE_CASE), 5)
+
+        
 
     # Rafraîchissement de l'affichage
     pygame.display.flip()
+        
 
 # Fin du programme
