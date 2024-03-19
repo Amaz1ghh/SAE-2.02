493035803c2b47a27453f4cb070413b844071fc2
igallea <igor.gallea@etudiant.univ-rennes1.fr>
2024-03-19 12:28:44 +0100

    huihuieshfuis

--- pygameTest1.py
+++ pygameTest1.py
@@ -3,50 +3,64 @@ import sys
 
 # Initialisation de Pygame
 pygame.init()
+clock = pygame.time.Clock()
+
+# variable temp
+tabtemp = [5, 14, 25, 18, 21, 12, 1, 8, 17, 6, 3, 10, 19, 22, 11, 2, 9, 20, 23, 16, 7, 4, 13, 24, 15]
+indiceCurentCase = 0
 
 # nb de cases
 LONGUEUR = 5
-LARGEUR = 5
+HAUTEUR = 5
 
 # Définition de la taille de la fenêtre
 TAILLE_CASE = 101
 
-#variable de positionnement du cavalier
+# Variable de positionnement du cavalier
 estPose = False
 x = 0
 y = 0
+
+# Tableau des flèches pour le parcours du cavalier
+tabFl = []
 
 # Couleurs
 blanc = (210, 180, 140)
 noir = (139, 69, 19)
 vert = (0, 255, 0)
 
-#image
+# Image
 image = pygame.image.load("cavalier.png")
 image = pygame.transform.scale(image, (TAILLE_CASE, TAILLE_CASE))
 
 
 # fonction qui place le cavalier
 
-def initPoseCavalier(x,y):
-    casex = x // TAILLE_CASE
-    casey = y // TAILLE_CASE
-    return (casex*TAILLE_CASE,casey*TAILLE_CASE)
+def deplacerCavalier(caseIndice):
+    indicex = (caseIndice-1) % LONGUEUR * TAILLE_CASE
+    indicey = (caseIndice-1) // LONGUEUR * TAILLE_CASE
+    return (indicex, indicey)
+
+def centreCase(caseCoord):
+    indicex = caseCoord[0] + TAILLE_CASE/2
+    indicey = caseCoord[1] + TAILLE_CASE/2
+    return (indicex, indicey)
+
 
 
 # Création de la fenêtre
-fenetre = pygame.display.set_mode((TAILLE_CASE*LONGUEUR, TAILLE_CASE*LARGEUR))
+fenetre = pygame.display.set_mode((TAILLE_CASE*LONGUEUR, TAILLE_CASE*HAUTEUR))
 pygame.display.set_caption("Échiquier Vierge")
 
 # Boucle principale
 while True:
 
     # Dessin de l'échiquier
-    fenetre.fill(blanc)
+    fenetre.fill((0, 0, 0))
     for ligne in range(LONGUEUR):
-        for colonne in range(LARGEUR):
+        for colonne in range(HAUTEUR):
             couleur_case = blanc if (ligne + colonne) % 2 == 0 else noir
-            pygame.draw.rect(fenetre, couleur_case, (colonne * TAILLE_CASE, ligne * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE))
+            pygame.draw.rect(fenetre, couleur_case, (ligne * TAILLE_CASE, colonne * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE))
 
     for event in pygame.event.get():
 
@@ -55,7 +69,6 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN and not estPose and event.button == 1:
-            x,y = initPoseCavalier(event.pos[0], event.pos[1])
             estPose = True
 
         if event.type == pygame.MOUSEMOTION and not estPose:
@@ -63,14 +76,31 @@ while True:
             y = event.pos[1]
             
     if estPose:
-        fenetre.blit(image, (x,y))
+        fenetre.blit(image, deplacerCavalier(tabtemp[indiceCurentCase]))
+        if indiceCurentCase < len(tabtemp) and indiceCurentCase > 0 :
+                pos1 = centreCase(deplacerCavalier(tabtemp[indiceCurentCase-1]))
+                pos2 = centreCase(deplacerCavalier(tabtemp[indiceCurentCase]))
+                tabFl.append((pos1, pos2))
+        if indiceCurentCase < len(tabtemp) - 1:
+            indiceCurentCase+=1
+        
+
     elif x != 0 and y != 0:
         pygame.draw.rect(fenetre, vert,(x//TAILLE_CASE*TAILLE_CASE,y//TAILLE_CASE*TAILLE_CASE, TAILLE_CASE,TAILLE_CASE), 5)
-
-        
-
+    
+    # Dessin du chemin
+    for fl in tabFl:
+        pygame.draw.line(fenetre, vert, fl[0], fl[1], 3)
+    
+    
     # Rafraîchissement de l'affichage
     pygame.display.flip()
-        
+
+    if estPose:
+        clock.tick(3)
+    else:
+        clock.tick(60)
+
+
 
 # Fin du programme
